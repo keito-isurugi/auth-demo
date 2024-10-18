209d7179e5a13ffed347ecb07c62229dcad642cc
keito-isurugi <ja224kg@gmail.com>
2024-10-18 23:02:20 +0900

    cookieからtokenを取得するよう修正

--- middleware/jwt_auth.go
+++ middleware/jwt_auth.go
@@ -11,18 +11,26 @@ import (
 func JWTAuth(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Authorization ヘッダーからトークンを取得
-		authHeader := r.Header.Get("Authorization")
-		if authHeader == "" {
-			http.Error(w,"Missing Authorization Header", http.StatusUnauthorized)
+		// authHeader := r.Header.Get("Authorization")
+		// fmt.Println(authHeader)
+		// if authHeader == "" {
+		// 	http.Error(w,"Missing Authorization Header", http.StatusUnauthorized)
+		// 	return
+		// }
+		
+		// "Bearer トークン" 形式なので、"Bearer " を取り除く
+		// tokenString := authHeader[len("Bearer "):]
+		cookie, err := r.Cookie("token")
+		if err != nil {
+			http.Error(w, "Missing token", http.StatusUnauthorized)
 			return
 		}
-
-		// "Bearer トークン" 形式なので、"Bearer " を取り除く
-		tokenString := authHeader[len("Bearer "):]
+		tokenString := cookie.Value
 
 		// トークンの検証
 		token, err := helper.ValidateJWT(tokenString)
 		if err != nil {
+			fmt.Println(token, err)
 			http.Error(w, "Invalid token", http.StatusUnauthorized)
 			return
 		}
@@ -30,9 +38,11 @@ func JWTAuth(next http.HandlerFunc) http.HandlerFunc {
 		// クレームからユーザーID(sub)を取得
 		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 			userID := claims["sub"].(string)
+			fmt.Println(userID)
 
 			// レスポンスにユーザーIDを返す
-			w.Write([]byte(fmt.Sprintf("UserID: %s", userID)))
+			// w.Write([]byte(fmt.Sprintf("UserID: %s", userID)))
+			next(w, r)
 		} else {
 			http.Error(w, "Invalid token", http.StatusUnauthorized)
 			return
