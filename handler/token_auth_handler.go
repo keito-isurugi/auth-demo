726ebb1c2e1f33ca14046fd32cee6010fe5db600
keito-isurugi <ja224kg@gmail.com>
2024-10-20 11:51:56 +0900

    jwtログイン処理実装

--- handler/token_auth_handler.go
+++ handler/token_auth_handler.go
@@ -3,8 +3,13 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/keito-isurugi/auth-demo/helper"
+	"github.com/keito-isurugi/auth-demo/infra"
+	"gorm.io/gorm"
 )
 
 func GetToken(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +26,7 @@ func GetToken(w http.ResponseWriter, r *http.Request) {
 
 func ValidToken(w http.ResponseWriter, r *http.Request) {
 	jwt := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjkyNjIyMTAsInN1YiI6IjEyMzQifQ.XRANbDR5Ow12vKXDC62YPSb9BC1cRd7SBzHWD5ciuAk"
-	token ,err := helper.ValidateJWT(jwt)
+	token, err := helper.ValidateJWT(jwt)
 	if err != nil {
 		fmt.Println(err)
 		http.Error(w, "invalid jwt error", http.StatusInternalServerError)
@@ -30,4 +35,47 @@ func ValidToken(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(token)
 	w.Write([]byte("valid token"))
+}
+
+func JWTLogin(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		userID, _ := strconv.Atoi(r.FormValue("id"))
+		password, _ := helper.HashPassword(r.FormValue("password"))
+
+		user, err := infra.GetUser(db, userID)
+		if err != nil {
+			http.Error(w, "Failed to retrieve user", http.StatusInternalServerError)
+			return
+		}
+
+		if helper.CheckPasswordHash(user.Password, password) {
+			http.Error(w, "Authentication failed", http.StatusUnauthorized)
+			return
+		}
+		
+		// jwt発行
+		token, err := helper.GenerateJWT(strconv.Itoa(user.ID))
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, "GenerateJWT error", http.StatusInternalServerError)
+			return
+		}
+		// クッキーにjwtを保存
+		cookie := &http.Cookie{
+			Name:  "jwt_auth_key",
+			Value: token,
+		}
+		http.SetCookie(w, cookie)
+
+		// リフレッシュトークン発行、DBに保存
+		rt := uuid.New()
+		t := time.Now().Add(time.Hour * 2)
+		if err := infra.SaveRefreshTokens(db, userID, rt, t); err != nil {
+			fmt.Println(err)
+			http.Error(w, "リフレッシュトークン登録エラー", http.StatusUnauthorized)
+			return
+		}
+
+		w.Write([]byte("JWTログイン成功！"))
+	}
 }
