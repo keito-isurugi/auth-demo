5fba312be5bdafdeb0d23ce0ffd0a14c00320e8d
keito-isurugi <ja224kg@gmail.com>
2025-01-26 21:00:00 +0900

    middleware route

--- middleware/http_method.go
+++ middleware/http_method.go
@@ -11,3 +11,16 @@ func Post(next http.HandlerFunc) http.HandlerFunc {
 		next(w, r)
 	}
 }
+
+func GetWithResJson(next http.HandlerFunc) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		// ヘッダーにContent-Typeを設定
+		w.Header().Set("Content-Type", "application/json")
+
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		next(w, r)
+	}
+}
